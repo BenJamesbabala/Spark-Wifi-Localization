4043e89ac137fc5f3f5b2836cef899f9216d3b2c
Kingsfield <hongkant@gmail.com>
2016-09-05 17:06:33 +0800

    1,change api to spark 1.6x;2,complete strategy code

--- Localization.scala
+++ Localization.scala
@@ -1,11 +1,11 @@
-package org.apache.spark.examples
+//package org.apache.spark.examples
+package com.tencent.tdw.spark.interview
 
 import scala.collection.mutable
-import breeze.linalg.{squaredDistance, DenseVector, Vector}
-import org.apache.spark.sql.SparkSession
+import breeze.linalg._
+import org.apache.spark._
 import org.apache.spark.mllib.clustering.GaussianMixtureModel
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.util.MLUtils
 
 /**
   * Created by kingsfield on 2016/9/4.
@@ -19,9 +19,9 @@ object Localization {
   we use Weiszfeld's algorithm to avoid calculate gradients.
   https://en.wikipedia.org/wiki/Geometric_median
    */
-  def bestL1Predict(points: mutable.ArrayBuffer[DenseVector[Double]], iter: Int): Unit = {
+  def bestL1Predict(points: Array[DenseVector[Double]], iter: Int): DenseVector[Double] = {
     val smallDouble = 0.01
-    var ret=points(0)
+    var ret= new DenseVector(points(0).toArray)
     for (i <- 1 to iter) {
       var x = DenseVector.zeros[Double](2)
       var sumWeight=0.0
@@ -37,37 +37,30 @@ object Localization {
     return ret
   }
 
-  def parseNdPoints(raw: String, n: Int): mutable.ArrayBuffer[DenseVector[Double]] = {
-    val ret = mutable.ArrayBuffer[DenseVector[Double]]()
+  def parseNdPoints(raw: String, n: Int): Array[DenseVector[Double]] = {
     val split=raw.split(",")
     val size = (split.length/n).toInt
+    val ret = new Array[DenseVector[Double]](size)
     for (i<-0 to size) {
       val v=DenseVector.zeros[Double](n)
       for (k<-0 to n) {
         v(k)=split(i*n+k).toDouble
       }
-      ret.append(v)
+      ret(i)=v
     }
     return ret
   }
 
-  def oldTrainOffline(): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("oldTrainOffline")
-      .getOrCreate()
-
+  def oldTrainOffline(sc:SparkContext): Unit = {
     // raw is training data which is 'mac_i:x,y' each line
-    val raw = spark.sparkContext.textFile("hdfs://path")
+    val raw = sc.textFile("hdfs://path")
     val kv = raw.map(line=>{
       val ss = line.split(";")
-      (ss(0),ss(1))
+      (ss(0),parseNdPoints(ss(1),2)(0))
     })
-    val training = kv.groupByKey().map(row=>{
-      (row._1,parseNdPoints(row._2.toString(),2))
-    })
+    val training = kv.groupByKey()
     val model = training.map(row=>{
-      (row._1,bestL1Predict(row._2,iteration))
+      (row._1,bestL1Predict(row._2.toArray,iteration))
     })
     model.saveAsTextFile("hdfs://path")
   }
@@ -88,45 +81,40 @@ object Localization {
   /*
   assume we have already got all mac model which is mac->(x,y)
    */
-  def oldValidationOffline(model : mutable.HashMap[String,DenseVector[Double]]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("oldTrainOffline")
-      .getOrCreate()
-
+  def oldValidationOffline(sc:SparkContext, model : mutable.HashMap[String,DenseVector[Double]]): Unit = {
     // raw is localization context, which is 'real_x,real_y,mac1,mac2,mac3,...' each line
-    val raw = spark.sparkContext.textFile("hdfs://path")
+    val raw = sc.textFile("hdfs://path")
+    val broadcastModel = sc.broadcast(model)
     val validation = raw.map(line=>{
-      parseValidation2d(line,model)
+      parseValidation2d(line,broadcastModel.value)
     }).map(row=>{
-      (row._1,bestL1Predict(row._2,iteration))
+      (row._1,bestL1Predict(row._2.toArray,iteration))
     }).map(row=>{
-//      val diff = row._1-row._2
-//      diff.asInstanceOf[DenseVector].length
+      val diff = row._1-row._2
+      norm(diff)
     }).collect()
     // then calculate the result off validation
-
   }
 
-  def new1000kMillionDollarsMethodTrain(): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("oldTrainOffline")
-      .getOrCreate()
-
+  def new1000kMillionDollarsMethodTrain(sc:SparkContext): Unit = {
     // raw is training data which is 'mac_i:x,y,signal' each line
-    val raw = spark.sparkContext.textFile("hdfs://path")
-    val kv = raw.map(row=>{
-      val split=row.split(":")
-      (split(0),split(1))
+    val raw = sc.textFile("hdfs://path")
+    val kv = raw.map(line=>{
+      val ss = line.split(";")
+      (ss(0),parseNdPoints(ss(1),3)(0))
     })
     val training = kv.groupByKey()
-
+    val model = training.map(row=>{
+      // train a gmm locally
+      // we omit training process for simplicity which it's easy to code a locally trained gmm model
+      // val model=new GaussianMixtureModel()
+    })
+    model.saveAsTextFile("hdfs://path")
   }
 
   def gmmProb(model: GaussianMixtureModel, point: DenseVector[Double]): Double = {
     val p = model.weights.zip(model.gaussians).map {
-      case (weight, dist) => fakeZero + weight * dist.pdf(Vectors.fromBreeze(point))
+      case (weight, dist) => fakeZero + weight * dist.pdf(Vectors.dense(point.toArray))
     }
     return p.sum
   }
@@ -142,7 +130,7 @@ object Localization {
     val samplePoints = new mutable.ArrayBuffer[DenseVector[Double]]()
     for (model <- models) {
       for (gaussian <- model.gaussians) {
-        samplePoints.append(gaussian.mu.asBreeze.asInstanceOf[DenseVector[Double]])
+        samplePoints.append(DenseVector(gaussian.mu.toArray))
       }
     }
     var sumWeigth=0.0
@@ -160,32 +148,45 @@ object Localization {
     return ret
   }
 
-  def new1000kMillionDollarsMethodValidation( model: mutable.HashMap[String,GaussianMixtureModel]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("new1000kMillionDollarsMethodValidation")
-      .getOrCreate()
-
+  def new1000kMillionDollarsMethodValidation(sc:SparkContext, model: mutable.HashMap[String,GaussianMixtureModel]): Unit = {
     // raw is localization context, which is 'real_x,real_y,mac1,mac2,mac3,...' each line
-    val raw = spark.sparkContext.textFile("hdfs://path")
+    val raw = sc.textFile("hdfs://path")
+    val broadcastModel = sc.broadcast(model)
     val validation = raw.map(line=>{
       val split = line.split(",")
       val real = DenseVector[Double](split(0).toDouble,split(1).toDouble)
       val sz = split.length-2
-      val predict = newPredict(split.takeRight(sz).mkString(","),model)
+      val predict = newPredict(split.takeRight(sz).mkString(","),broadcastModel.value)
       (real,predict)
     }).map(row=>{
       val diff = row._1 - row._2
-      (diff.length)
+      norm(diff)
     }).collect()
     // then calculate the result off validation
   }
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("oldTrainOffline")
-      .getOrCreate()
-    var rdd1 = spark.sparkContext.makeRDD(Array(("A",0),("A",2),("B",1),("B",2),("C",1)))
+    val sparkConf_ = new SparkConf()
+    sparkConf_.setAppName("InterviewShowcase")
+    sparkConf_.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    sparkConf_.set("spark.default.parallelism", "1000")
+    sparkConf_.set("spark.storage.memoryFraction", "0.5")
+    sparkConf_.set("spark.shuffle.memoryFraction", "0.3")
+    sparkConf_.set("spark.shuffle.memoryFraction", "0.3")
+    sparkConf_.set("spark.driver.memory", "16g")
+    sparkConf_.set("spark.driver.maxResultSize", "12g")
+    val sc = new SparkContext(sparkConf_)
+
+    // old method
+    oldTrainOffline(sc)
+    // suppose we have load models from oldTrainOffline
+    val oldModels = new mutable.HashMap[String,DenseVector[Double]] ()
+    oldValidationOffline(sc,oldModels)
+
+    // new method
+    new1000kMillionDollarsMethodTrain(sc)
+    // suppose we have load models from new1000kMillionDollarsMethodTrain
+    val newModels = new mutable.HashMap[String,GaussianMixtureModel]()
+    new1000kMillionDollarsMethodValidation(sc,newModels)
   }
 }
